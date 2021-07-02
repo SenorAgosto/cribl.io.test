086826087d2d6d6587a1eb91e2f714c44022e134
Austin Gilbert <austin.gilbert@gmail.com>
2021-07-02 15:37:32 -0500

    fleshing out the web api boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/log/<log_name>/<num_lines>")]
+fn get_log(log_name : String, num_lines : u64) -> String {
+	format!("log_name: {}, num_lines: {}", log_name, num_lines)
+}
+
 fn main() {
-    println!("Hello, world!");
+	rocket::ignite().mount("/", routes![get_log]).launch();
 }
