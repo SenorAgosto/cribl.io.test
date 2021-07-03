2da6d545c11896a48d24822c8e0583f0100ffc04
Austin Gilbert <austin.gilbert@gmail.com>
2021-07-03 14:25:55 -0500

    fleshing out log_reader()

--- src/log_reader.rs
+++ src/log_reader.rs
@@ -1,18 +1,41 @@
 use std::fs::{File, OpenOptions};
 use std::io::{self, prelude::*, BufReader, Error, ErrorKind};
 use std::result::Result;
+use circular_queue::CircularQueue;
 
 pub fn read_log(file_name : String, num_lines : u64, filter : Option<String> ) -> Result<String, io::Error> {
 
-	let mut lines : Vec<String> = Vec::with_capacity(num_lines as usize); 
-	lines.resize(num_lines as usize, "".to_string());
+	let mut lines = CircularQueue::<String>::with_capacity(num_lines as usize);
 
 	let file = File::open(file_name)?;
 	let reader = BufReader::new(file);
 
-	for line in reader.lines() {
+	let pattern = match filter {
+		Some(v) => v,
+		None => "".to_string(),
+	};
+
+	for line_iter in reader.lines() {
+
+		let line = line_iter.unwrap();
+		
+		if(pattern.is_empty()) {
+
+			lines.push(line);
+
+		} else {
+
+			if(line.contains(&pattern)) {
+				lines.push(line);
+			}	
+		}
 	}
 
-	Ok(lines.join("\n"))
-} 
+	let mut r = "".to_string();
+	for line in lines.iter() {
 
+		r = r + line;
+	}
+	
+	Ok(r)
+} 
