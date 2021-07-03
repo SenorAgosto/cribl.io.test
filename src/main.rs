4e50867741f1b18a412a40f0d760c36bd6ac388a
Austin Gilbert <austin.gilbert@gmail.com>
2021-07-03 14:51:26 -0500

    tying route handler to log reading functionality by calling read_log()

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,28 @@ use log_reader::read_log;
 
 #[get("/log/<log_name>/<num_lines>")]
 fn get_log(logs : State<LogPath>, log_name : String, num_lines : u64) -> String {
-	format!("log_path: {}, log_name: {}, num_lines: {}", logs.path, log_name, num_lines)
+	
+	let mut path = logs.path.clone();
+	path.push_str("/");
+	path = path + &log_name;
+
+	match read_log(path.to_string(), num_lines, None) {
+		Ok(r) => r,
+		Err(e) => panic!("{}", e),
+	}
 }
 
 #[get("/filtered_log/<log_name>/<filter>/<num_lines>")]
 fn get_filtered_log(logs : State<LogPath>, log_name : String, filter : String, num_lines : u64) -> String {
-	format!("log_path: {}, log_name: {}, filter: {},  num_lines: {}", logs.path, log_name, filter, num_lines)
+
+	let mut path = logs.path.clone();
+	path.push_str("/");
+	path = path + &log_name;
+
+	match read_log(path, num_lines, Some(filter)) {
+		Ok(r) => r,
+		Err(e) => panic!("{}", e),
+	}
 }
 
 fn main() {
