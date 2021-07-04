f5ee0083b0402565dddccfadcaab74ddf2e70053
Austin Gilbert <austin.gilbert@gmail.com>
2021-07-03 23:03:03 -0500

    behave: fleshing out step functions

--- features/steps/log_collection_steps.py
+++ features/steps/log_collection_steps.py
@@ -1,3 +1,10 @@
+import log_collection as log
+import requests
+
+def before_scenario(context, scenario):
+	print("performing setup")
+
+
 @given(u'log_collection is running')
 def cargo_run(context):
 	pass
@@ -8,18 +15,26 @@ def capture_expected_result(context):
 
 @when(u'we request {num_lines} lines from {log_file} with filter keyword {keyword}')
 def request_filtered_log(context, num_lines, log_file, keyword):
-	pass
+	url = log.filtered_log_uri(log_file, keyword, num_lines)
+	response = requests.get(url)
+
+	context.status_code = response.status_code
+	context.result = response.text.strip()
 
 @when(u'we request {num_lines} lines from {log_file}')
 def request_log(context, num_lines, log_file):
-	pass
+	url = log.log_uri(log_file, num_lines)
+	response = requests.get(url)
+
+	context.status_code = response.status_code
+	context.result = response.text.strip()
 
 @then(u'we get the expected result')
 def assert_result(context):
-	result = ""
-	assert context.expected_result == result 
+	assert context.status_code == 200
+	assert context.expected_result == context.result
 
 @then(u'we get a server error')
 def assert_error(context):
-	pass
+	assert context.status_code == 500
 
