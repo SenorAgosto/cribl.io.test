db0635e597558c1cff8f908692034bfaac8ecbe0
Austin Gilbert <austin.gilbert@gmail.com>
2021-07-08 12:42:31 -0500

    perf: continue improving microservice performance, Issue #1
    Use Boyer-Moore string search algorithm when checking for simple keyword filter. The performance should improve as the filter keyword length increases. Split the filtered and non-filtered log reading function to eliminate branching.
    
    Testing done on a 2.1GB log on SSD with _no_ keyword matches (worst case scenario):
    
    Original implementation using std::String.contains(), as the filter keyword increases in size, the query time grows:
    
        $ time wget http://localhost:8888/filtered_log/big.log/MayAmber/100
        real    0m9.583s
        user    0m0.004s
        sys     0m0.006s
    
        $ time wget http://localhost:8888/filtered_log/big.log/MayAmberJuneJulyAugust/100
        real    0m10.477s
        user    0m0.004s
        sys     0m0.005s
    
    Using Boyer-Moore, as the filter keyword increases in size, the query time shrinks:
    
        $ time wget http://localhost:8888/filtered_log/big.log/MayAmber/100
        real    0m6.162s
        user    0m0.004s
        sys     0m0.006s
    
        $ time wget http://localhost:8888/filtered_log/big.log/MayAmberJuneJulyAugust/100
        real    0m5.448s
        user    0m0.003s
        sys     0m0.005s

--- src/log_reader.rs
+++ src/log_reader.rs
@@ -1,12 +1,23 @@
+use needle::BoyerMoore;
 use std::fs::File;
 use std::io::{Error, ErrorKind};
 use std::result::Result;
 use memmap::MmapOptions;
 
-fn line_from_mmap(slice : &[u8]) -> Result<String, std::io::Error> {
+fn capture_line(pattern : &BoyerMoore<[u8]>, line : &[u8]) -> bool {
 
+	if line.len() == 0 {
+		return false;
+	}
+
+	pattern.find_in(line).next().is_some()
+}
+
+
+fn line_from_bytes(slice : &[u8]) -> Result<String, std::io::Error> {
+	
 	match std::str::from_utf8(slice).to_owned() {
-		Ok(v) => return Ok(v.to_string()),
+		Ok(v)   => return Ok(v.to_string()),
 		Err(_e) => return Err(Error::new(ErrorKind::Other, "utf-8 conversion error")),
 	}
 }
@@ -23,34 +34,73 @@ fn collect_lines(lines : &Vec::<String>, capacity : usize) -> Result<String, std
 	Ok(r)
 }
 
-pub fn read_log(file_name : String, num_lines : u64, filter : Option<String> ) -> Result<String, std::io::Error> {
+// TODO: read_log_without_filter() and read_log_with_filter() could probably be 
+// a function template
+fn read_log_without_filter(file_name : &String, num_lines : usize) -> Result<String, std::io::Error> {
 
-	let num_lines : usize = num_lines as usize;
-	let mut lines = Vec::<String>::with_capacity(num_lines as usize);
-
-	let file = File::open(file_name)?;
+	let mut lines = Vec::<String>::with_capacity(num_lines);
+	let file = File::open(&file_name)?;
 	let mmap = unsafe { MmapOptions::new().map(&file)? };
 
-	let pattern = match filter {
-		Some(v) => v,
-		None => "".to_string(),
-	};
+	// TODO: handle the case the log is only 2 bytes
+	assert!(mmap.len() >= 2);
 
 	let mut collected_lines : usize = 0;
 	let mut prev_spot : usize = mmap.len() - 1;
-	let mut spot : usize = mmap.len() - 2; // skip the last character of the buffer in case it's '\n'
+	let mut spot : usize = mmap.len() - 2; // skip the last char in the buffer in case it's '\n'
 
-	while spot > 0 && collected_lines < num_lines {	
+	while spot > 0 && collected_lines < num_lines {
 
 		if mmap[spot] == b'\n' {
+			let line = &mmap[spot + 1..prev_spot];
+			let s = line_from_bytes(line)?;
 
-			let line = line_from_mmap(&mmap[spot + 1..prev_spot])?;
+			lines.push(s);
+			collected_lines += 1;
 
-			if pattern.is_empty() {
-				lines.push(line.to_string()); 
-				collected_lines += 1;
-			} else if line.contains(&pattern) {
-				lines.push(line.to_string());
+			prev_spot = spot;
+		}
+
+		spot -= 1;
+	};
+	
+	// grab the first line of the log if we haven't collect enough lines
+	if spot == 0 && collected_lines < num_lines {
+
+		let line = &mmap[spot..prev_spot];
+		let s = line_from_bytes(line)?;
+		
+		lines.push(s);
+	}
+
+	let capacity = mmap.len() - spot + 1;
+	collect_lines(&lines, capacity)
+}
+
+fn read_log_with_filter(file_name : &String, filter : &String, num_lines : usize ) -> Result<String, std::io::Error> {
+	
+	let mut lines = Vec::<String>::with_capacity(num_lines);
+	let file = File::open(&file_name)?;
+	let mmap = unsafe { MmapOptions::new().map(&file)? };
+
+	// TODO: handle the case the log is only 2 bytes
+	assert!(mmap.len() >= 2);
+
+	let pattern = BoyerMoore::new(filter.as_str().as_bytes());
+
+	let mut collected_lines : usize = 0;
+	let mut prev_spot : usize = mmap.len() - 1;
+	let mut spot : usize = mmap.len() - 2;
+
+	while spot > 0 && collected_lines < num_lines {
+		
+		if mmap[spot] == b'\n' {
+			let line = &mmap[spot + 1..prev_spot];
+
+			if capture_line(&pattern, &line) {
+				let s = line_from_bytes(line)?;
+				lines.push(s);
+
 				collected_lines += 1;
 			}
 
@@ -59,19 +109,28 @@ pub fn read_log(file_name : String, num_lines : u64, filter : Option<String> ) -
 
 		spot -= 1;
 	};
-
+	
 	// grab the first line of the log if we haven't collect enough lines
 	if spot == 0 && collected_lines < num_lines {
-		let line = line_from_mmap(&mmap[spot..prev_spot])?;	
 
-		if pattern.is_empty() {
-			lines.push(line.to_string());
-		} else if line.contains(&pattern) {
-			lines.push(line.to_string());
-			
+		let line = &mmap[spot..prev_spot];
+
+		if capture_line(&pattern, &line) {
+			let s = line_from_bytes(line)?;
+			lines.push(s);
 		}
 	}
 
 	let capacity = mmap.len() - spot + 1;
 	collect_lines(&lines, capacity)
+}
+
+pub fn read_log(file_name : String, num_lines : u64, filter : Option<String> ) -> Result<String, std::io::Error> {
+
+	let num_lines : usize = num_lines as usize;
+
+	match filter {
+		Some(v)  => read_log_with_filter(&file_name, &v, num_lines),
+		None	 => read_log_without_filter(&file_name, num_lines),
+	}
 } 
